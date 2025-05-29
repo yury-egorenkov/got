10c62daaf05a7338887b9c698501c731c524e9b0
Yury Egorenkov <yury.egorenkov@gmail.com>
2025-05-29 20:07:45 +0200

    refactor CLI to use native flag parsing and standardize test naming
    * Removes `mitranim/cmd` dependency in favor of gg.FlagParseTo for simpler CLI architecture.
    * Renames test files to follow t_ prefix convention.
    * Add print help.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	l "log"
 	"os"
 	"path"
 	"path/filepath"
@@ -14,7 +15,6 @@ import (
 	"text/template/parse"
 
 	"github.com/joho/godotenv"
-	"github.com/mitranim/cmd"
 	"github.com/mitranim/gg"
 )
 
@@ -27,39 +27,85 @@ type (
 )
 
 var (
-	// CLI commands. Mutated by `init` functions.
-	commands = cmd.Map{
-		`run`: CmdRunner[CmdTempl]{}.Run,
-	}
+	log = l.New(os.Stderr, `[got] `, 0)
+	cwd = gg.Cwd()
 )
 
-type CmdRunner[_ gg.Runner] struct{}
+func OptDefault() Opt { return gg.FlagParseTo[Opt](nil) }
 
-func (CmdRunner[A]) Run() { gg.FlagParseTo[A](cmd.Args()).Run() }
+type Opt struct {
+	Args           []string `flag:""`
+	Help           bool     `flag:"-h" desc:"Print help and exit."`
+	OutputFileName string   `flag:"-o" desc:"Output path to file. Stdout if not set."`
+}
+
+func (self *Opt) Init(src []string) {
+	err := gg.FlagParseCatch(src, self)
+	if err != nil {
+		self.LogErr(err)
+		gg.Write(log.Writer(), gg.Newline)
+		self.PrintHelp()
+		os.Exit(1)
+	}
+
+	if self.Help || gg.Head(self.Args) == `help` {
+		self.PrintHelp()
+		os.Exit(0)
+	}
+
+	if len(self.Args) != 1 {
+		self.PrintHelp()
+		os.Exit(1)
+	}
+}
+
+func (self Opt) PrintHelp() {
+	gg.FlagFmtDefault.Prefix = "\t"
+	gg.FlagFmtDefault.Head = false
+
+	gg.Nop2(fmt.Fprintf(
+		log.Writer(),
+		TrimLines(`
+"got" is the missing Go template processing cli tool.
+Use any text file as Go tmplate, inline values from env, call funcs and more.
+
+Usage:
+
+	got <got_flags> <path_to_template>
+
+Flags:
+%v
+`),
+		gg.FlagHelp[Opt](),
+	))
+}
+
+func (self Opt) LogErr(err error) {
+	if err != nil {
+		log.Printf(`%+v`, err)
+	}
+}
+
+type Main struct {
+	Opt
+	Idents []string
+}
 
 func main() {
 	defer gg.RecWith(fatal)
 	initEnvVars()
 
-	// flags
-	// jsonFmt
-
-	cmd := commands.Get()
-	cmd()
+	var main Main
+	main.Opt.Init(os.Args[1:])
+	main.Run()
 }
 
 func fatal(err error) {
-	fmt.Println(`Error:`, err)
+	log.Printf(`%+v`, err)
 	os.Exit(1)
 }
 
-type CmdTempl struct {
-	TemplFileName  string `flag:"-t" desc:"Path of a template file."`
-	OutputFileName string `flag:"-o" desc:"Path to output file. Stdout if unset."`
-	Idents         []string
-}
-
-func (self CmdTempl) Templ() *Templ {
+func (self Main) Templ() *Templ {
 	funcs := template.FuncMap{
 		`GetEnv`:         GetEnv,
 		`Indent`:         Indent,
@@ -70,24 +116,24 @@ func (self CmdTempl) Templ() *Templ {
 	return template.New(`templ`).Funcs(funcs)
 }
 
-func (self CmdTempl) Run() {
+func (self Main) Run() {
 	out := os.Stdout
 	if gg.IsNotZero(self.OutputFileName) {
-		outPathName := path.Join(gg.Cwd(), self.OutputFileName)
+		outPathName := path.Join(cwd, self.OutputFileName)
 		file := gg.Try1(os.OpenFile(outPathName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644))
 		defer file.Close()
 
 		out = file
 	}
 
-	body := gg.ReadFile[string](self.TemplFileName)
+	body := gg.ReadFile[string](self.Opt.Args[0])
 	self.Idents = self.IdentsByTemplateAST(body, `ReadFileIndent`)
 
 	templ := gg.Try1(self.Templ().Parse(body))
 	gg.Try(templ.Execute(out, nil))
 }
 
-func (self *CmdTempl) ReadFileIndent(name string) (out string) {
+func (self *Main) ReadFileIndent(name string) (out string) {
 	body := ReadFile(name)
 	ident := gg.Newline + self.Idents[0]
 	out = strings.Replace(body, gg.Newline, ident, -1)
@@ -105,8 +151,10 @@ func IdentsByFuncName(body string, name string) (out []string) {
 			// Use first captured group if available, otherwise second
 			if val[1] != "" {
 				out = append(out, val[1])
-			} else if len(val) > 2 && val[2] != "" {
-				out = append(out, val[2])
+			} else {
+				if len(val) > 2 && val[2] != "" {
+					out = append(out, val[2])
+				}
 			}
 		}
 	}
@@ -115,9 +163,10 @@ func IdentsByFuncName(body string, name string) (out []string) {
 }
 
 // Parse template AST to find indentation context
-func (self CmdTempl) IdentsByTemplateAST(body string, name string) (out []string) {
+func (self Main) IdentsByTemplateAST(body string, name string) (out []string) {
 	templ := gg.Try1(self.Templ().Parse(body))
 	lines := strings.Split(body, "\n")
+
 	tree := templ.Tree
 	if tree != nil && tree.Root != nil {
 		walkNodes(tree.Root, lines, name, &out)
@@ -131,11 +180,12 @@ func walkNodes(node parse.Node, lines []string, funcName string, indents *[]stri
 		return
 	}
 
-	switch n := node.(type) {
+	switch nod := node.(type) {
+
 	case *parse.ActionNode:
-		if containsFunction(n.Pipe, funcName) {
+		if containsFunction(nod.Pipe, funcName) {
 			// Find line number and extract indentation
-			pos := int(n.Position())
+			pos := int(nod.Position())
 			lineNum := findLineNumber(lines, pos)
 			if lineNum >= 0 && lineNum < len(lines) {
 				line := lines[lineNum]
@@ -143,21 +193,25 @@ func walkNodes(node parse.Node, lines []string, funcName string, indents *[]stri
 				*indents = append(*indents, indent)
 			}
 		}
+
 	case *parse.ListNode:
-		if n != nil {
-			for _, child := range n.Nodes {
+		if nod != nil {
+			for _, child := range nod.Nodes {
 				walkNodes(child, lines, funcName, indents)
 			}
 		}
+
 	case *parse.IfNode:
-		walkNodes(n.List, lines, funcName, indents)
-		walkNodes(n.ElseList, lines, funcName, indents)
+		walkNodes(nod.List, lines, funcName, indents)
+		walkNodes(nod.ElseList, lines, funcName, indents)
+
 	case *parse.RangeNode:
-		walkNodes(n.List, lines, funcName, indents)
-		walkNodes(n.ElseList, lines, funcName, indents)
+		walkNodes(nod.List, lines, funcName, indents)
+		walkNodes(nod.ElseList, lines, funcName, indents)
+
 	case *parse.WithNode:
-		walkNodes(n.List, lines, funcName, indents)
-		walkNodes(n.ElseList, lines, funcName, indents)
+		walkNodes(nod.List, lines, funcName, indents)
+		walkNodes(nod.ElseList, lines, funcName, indents)
 	}
 }
 
@@ -268,4 +322,8 @@ func CommaSplit[A gg.Text](src A) []A {
 
 func IsErrFileNotFound(err error) bool {
 	return errors.Is(err, os.ErrNotExist)
+}
+
+func TrimLines(src string) string {
+	return strings.Trim(src, "\n")
 }
